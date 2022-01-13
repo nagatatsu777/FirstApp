9298b2d9868a816615fa0bdf37f9d9ce9d8f5f9c
nagatatsu77 <ntkyl77@gmail.com>
2022-01-13 17:36:14 -0500

    Project 1 Challenge Final

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,23 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    let unitSelection = ["seconds","minutes","hours","days"]
-    @State private var originSelectedItem = "seconds"
+    let unitSelection = ["seconds","minutes","hours"]
     @State private var selectedItem = "seconds"
     @State private var amount = 0
+    var result: Int{
+        let amo = amount
+        let selected = selectedItem
+        switch selected{
+        case "seconds":
+            return amo
+        case "minutes":
+            return amo/60
+        case "hours":
+            return amo/3600
+        default:
+            return amo
+        }
+    }
     var body: some View {
         NavigationView{
         Form{
-            Picker("Original Unit", selection: $originSelectedItem){
-                ForEach(unitSelection, id: \.self){
-                    Text($0)
-                }
-            }
             TextField("Amount",value: $amount, format: .number)
+                .keyboardType(.decimalPad)
             Picker("Unit to be converted", selection:$selectedItem){
                 ForEach(unitSelection, id: \.self){
                     Text($0)
                 }
+            }
+            Section{
+                Text(result, format: .number)
+            }header: {
+                Text("Result")
             }
         }
         .navigationTitle("TEST")
